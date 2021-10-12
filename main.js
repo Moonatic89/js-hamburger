d2627eaec8623dd07b4a9e1cbc3b469641af010d
StefanoNesi <sn.boolean@gmail.com>
2021-10-12 16:37:50 +0200

    Finished

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 //Switch between hamburger menu open and close onclick. NOTE: There is an unused .active class in css/media query, which displays the menu itself.
 
-const openButton = document.querySelector("div > a");
-const hamburgerMenu = document.querySelector(".hamburger-menu");
 
+// Initialize Variables
+
+const openButton = document.querySelector("div > a");
+const closeButton = document.querySelector(".hamburger-menu > a");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
 
 //console.log(hamburgerMenu); // <- is working
 
+//Click event
+
 openButton.addEventListener("click", addActiveClass);
+closeButton.addEventListener("click", removeActiveClass);
+
+//Functions
 
 function addActiveClass() {
-
-    console.log(hamburgerMenu); // <- On click event working
-
+    //    console.log(hamburgerMenu); // <- On click event working
     hamburgerMenu.classList.add("active");
+}
 
-    //  classList.add("active"); <- not working
-    // document.getElementsByClassName("hamburger-menu").classList.add("active");
-
+function removeActiveClass() {
+    hamburgerMenu.classList.remove("active");
 }
 
